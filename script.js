6912d0bed1ea721b6a097829378c6c94025e3f97
Anish Nigam <117471360+nigam85@users.noreply.github.com>
2023-09-18 21:47:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,45 @@
-function convertToRoman(num) {
-  	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
-    };
+// function convertToRoman(num) {
+//   	const obj = {
+//       0:['M',1000], 
+//       1:['D', 500], 
+//       2:['C', 100], 
+//       3:['L', 50], 
+//       4:['X', 10], 
+//       5:['V', 5], 
+//       6:['I', 1]
+//     };
 
-  //your code here
-	let result = '';
+//   //your code here
+// 	let result = '';
+
+//     for (const symbol of romanSymbols) {
+//         while (num >= symbol.value) {
+//             result += symbol.symbol;
+//             num -= symbol.value;
+//         }
+//     }
+
+//     return result;
+
+// }
+function intToRoman(num) {
+    const romanSymbols = [
+        { value: 1000, symbol: 'M' },
+        { value: 900, symbol: 'CM' },
+        { value: 500, symbol: 'D' },
+        { value: 400, symbol: 'CD' },
+        { value: 100, symbol: 'C' },
+        { value: 90, symbol: 'XC' },
+        { value: 50, symbol: 'L' },
+        { value: 40, symbol: 'XL' },
+        { value: 10, symbol: 'X' },
+        { value: 9, symbol: 'IX' },
+        { value: 5, symbol: 'V' },
+        { value: 4, symbol: 'IV' },
+        { value: 1, symbol: 'I' }
+    ];
+
+    let result = '';
 
     for (const symbol of romanSymbols) {
         while (num >= symbol.value) {
@@ -20,7 +49,6 @@ function convertToRoman(num) {
     }
 
     return result;
-
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
