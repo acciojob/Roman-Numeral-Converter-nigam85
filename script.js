6e705f9d114231dddf234aed81091917e4049c7a
Anish Nigam <117471360+nigam85@users.noreply.github.com>
2023-09-19 08:47:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,61 +1,11 @@
-// function convertToRoman(num) {
-//   	const obj = {
-//       0:['M',1000], 
-//       1:['D', 500], 
-//       2:['C', 100], 
-//       3:['L', 50], 
-//       4:['X', 10], 
-//       5:['V', 5], 
-//       6:['I', 1]
-//     };
-
-//   //your code here
-// 	let result = '';
-
-//     for (const symbol of romanSymbols) {
-//         while (num >= symbol.value) {
-//             result += symbol.symbol;
-//             num -= symbol.value;
-//         }
-//     }
-
-//     return result;
-
-// }
-function intToRoman(num) {
-    const romanSymbols = [
-        { value: 1000, symbol: 'M' },
-        { value: 900, symbol: 'CM' },
-        { value: 500, symbol: 'D' },
-        { value: 400, symbol: 'CD' },
-        { value: 100, symbol: 'C' },
-        { value: 90, symbol: 'XC' },
-        { value: 50, symbol: 'L' },
-        { value: 40, symbol: 'XL' },
-        { value: 10, symbol: 'X' },
-        { value: 9, symbol: 'IX' },
-        { value: 5, symbol: 'V' },
-        { value: 4, symbol: 'IV' },
-        { value: 1, symbol: 'I' }
-    ];
-
-    let result = '';
-
-    for (const symbol of romanSymbols) {
-        while (num >= symbol.value) {
-            result += symbol.symbol;
-            num -= symbol.value;
-        }
+function convertToRoman(num) {
+  var ref = [['M', 1000], ['CM', 900], ['D', 500], ['CD', 400], ['C', 100], ['XC', 90], ['L', 50], ['XL', 40], ['X', 10], ['IX', 9], ['V', 5], ['IV', 4], ['I', 1]];
+  var res = [];
+  ref.forEach(function(p) {
+    while (num >= p[1]) {
+      res.push(p[0]);
+      num -= p[1];
     }
-
-    return result;
+  });
+  return res.join('');
 }
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
-
-// console.log(convertToRoman(36));
-
-
-
-
-// do not edit below this line
-module.exports = convertToRoman
