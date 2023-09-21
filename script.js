7619e8f58e02e799d29e8cd63ef07222d9481d45
Anish Nigam <117471360+nigam85@users.noreply.github.com>
2023-09-21 11:21:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-// function convertToRoman(num) {
-//   var ref = [['M', 1000], ['CM', 900], ['D', 500], ['CD', 400], ['C', 100], ['XC', 90], ['L', 50], ['XL', 40], ['X', 10], ['IX', 9], ['V', 5], ['IV', 4], ['I', 1]];
-//   var res = [];
-//   ref.forEach(function(p) {
-//     while (num >= p[1]) {
-//       res.push(p[0]);
-//       num -= p[1];
-//     }
-//   });
-//   return res.join('');
-// }
 var convertToRoman = function(num) {
   var decimalValue = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
   var romanNumeral = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
@@ -23,4 +12,4 @@ var convertToRoman = function(num) {
   }
 
   return romanized;
-};
+}; 
