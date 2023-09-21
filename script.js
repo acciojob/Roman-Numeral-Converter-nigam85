5f06e3ed92a93d109868df95781cf404c7afd24d
Anish Nigam <117471360+nigam85@users.noreply.github.com>
2023-09-21 12:26:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,37 @@
-var convertToRoman = function(num) {
-  var decimalValue = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
-  var romanNumeral = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
+function intToRoman(num) {
+  if (num <= 0 || num > 100000) {
+    return "Invalid input. Please enter a number between 1 and 100000.";
+  }
 
-  var romanized = "";
+  const romanNumerals = [
+    { value: 1000, symbol: "M" },
+    { value: 900, symbol: "CM" },
+    { value: 500, symbol: "D" },
+    { value: 400, symbol: "CD" },
+    { value: 100, symbol: "C" },
+    { value: 90, symbol: "XC" },
+    { value: 50, symbol: "L" },
+    { value: 40, symbol: "XL" },
+    { value: 10, symbol: "X" },
+    { value: 9, symbol: "IX" },
+    { value: 5, symbol: "V" },
+    { value: 4, symbol: "IV" },
+    { value: 1, symbol: "I" }
+  ];
 
-  for (var index = 0; index < decimalValue.length; index++) {
-    while (decimalValue[index] <= num) {
-      romanized += romanNumeral[index];
-      num -= decimalValue[index];
+  let result = "";
+
+  for (const numeral of romanNumerals) {
+    while (num >= numeral.value) {
+      result += numeral.symbol;
+      num -= numeral.value;
     }
   }
 
-  return romanized;
-}; 
+  return result;
+}
+
+// Example usage:
+const input = 798;
+const romanNumeral = intToRoman(input);
+console.log(romanNumeral); // Output: "DCCXCVIII"
